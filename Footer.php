57f067fd902bef35e4e7d30f8ce62b58f614d03d
vobaquan123 <137363737+vobaquan123@users.noreply.github.com>
2023-12-16 13:45:10 +0700

    Add files via upload

---
+++ Footer.php
@@ -0,0 +1,1 @@
+<div id="footer">  &copy; 2023 Online Clothing Store</div>
---
+++ Header.php
@@ -0,0 +1,15 @@
+<div id="title">
+    <h1><span style="color: #008000;">ONLINE CLOTHING STORE</span></h1>
+  </div>
+  
+<div class="container">
+    <ul id="navCircle">
+      <li><a class="active" href="index.php">Home</a></li>
+       <li><a href="User.php">User</a></li>
+      <li><a href="Category.php">Category</a></li>
+
+      <li><a href="Offers.php">Offers</a></li>
+ <li><a href="Orders.php">Orders</a></li>
+       <li><a href="Logout.php">Logout</a></li>
+    </ul>
+  </div>
